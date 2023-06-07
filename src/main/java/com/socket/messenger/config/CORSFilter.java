dec54b496665c6c81c744b91c70c6397e3274bd4
idean3885 <dykimdev3885@gmail.com>
2023-06-07 21:51:00 +0900

    fix/소켓 endpoint 변경, CORS 재수정

--- src/main/java/com/socket/messenger/config/CORSFilter.java
+++ src/main/java/com/socket/messenger/config/CORSFilter.java
@@ -18,7 +18,7 @@ public class CORSFilter implements Filter {
         var httpServletRequest = (HttpServletRequest) request;
         var httpservletResponse = (HttpServletResponse) response;
 
-        httpservletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://172.20.10.7:8863");
+        httpservletResponse.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://172.20.10.6:3000");
         httpservletResponse.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         httpservletResponse.setHeader("Access-Control-Allow-Credentials", "true");
--- src/main/java/com/socket/messenger/config/WebSocketConfig.java
+++ src/main/java/com/socket/messenger/config/WebSocketConfig.java
@@ -1,19 +1,19 @@
 package com.socket.messenger.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@EnableAutoConfiguration
+@Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/receive")
-                .setAllowedOrigins("*")
+        registry.addEndpoint("/")
+                .setAllowedOrigins("http://172.20.10.6:3000")
                 .withSockJS();
     }
 
